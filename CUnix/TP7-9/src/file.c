14703f5a8958d6072dca7a1373e35ec10d37e255
PremiumP <a.secret.patrice@gmail.com>
2017-12-05 17:48:00 +0100

    modification pour remove

--- CUnix/TP7-9/src/file.c
+++ CUnix/TP7-9/src/file.c
@@ -17,12 +17,10 @@ void remove_words_from_file(word_entry **, word_entry **, int);
 int is_word_loaded(char *, int, word_list *);
 void inc_times_word(char *, int, word_list *);
 
-
-
-
 //------------------------------------------------------------------------
 // global functions definitions
 //------------------------------------------------------------------------
+
 /**
    Create and initialize file table of capacity maxfiles
 
@@ -31,16 +29,19 @@ void inc_times_word(char *, int, word_list *);
 
    returns : pointer to table or NULL in case of error
  */
-listfile_entry *create_filelist(int maxfiles)
-{
-  listfile_entry *listfile_ptr = NULL;
-  listfile_ptr = (listfile_entry*)malloc(sizeof(listfile_entry) * maxfiles);
-  for (int i = 0; i < maxfiles; i++) {
-    strcpy(listfile_ptr[i].filename, "");
-    listfile_ptr[i].loaded = 0;
-  }
+listfile_entry *create_filelist(int maxfiles){
 
-  return listfile_ptr;
+    int i = 0;
+    listfile_entry *listfile_ptr = NULL;
+    listfile_ptr = (listfile_entry*)malloc(sizeof(listfile_entry) * maxfiles);
+
+    for(i = 0; i <MAX_FILES; i++){
+        strcpy(listfile_ptr[i].filename, "");
+        listfile_ptr[i].loaded = 0;
+    }
+
+    return listfile_ptr;
+
 }
 
 /**
@@ -62,46 +63,48 @@ listfile_entry *create_filelist(int maxfiles)
    -2 if allocation error
     0 if everything ok
 */
-int add_file(char filename[],listfile_entry * filelist, hash_table * htable_ptr)
-{
-  FILE *file;
-  char buffer[MAX_LENGTH];
-  int index = find_free_index(filelist,filename);
+int add_file(char filename[], listfile_entry * filelist, hash_table * htable_ptr) {
 
-  file = fopen(filename, "r");
+    FILE *file;
+    char buffer[MAX_LENGTH];
+    int index = find_free_index(filelist,filename);
+    int i = 0;
 
-	if(index == -1) {
+    file = fopen(filename, "r");
+
+    if(index == -1) {
 		return 1;
 	}
 	if(index == -2) {
 		return 2;
 	}
 
-  if(file == NULL) {
-    return -1;
-  }
-
-  add_filelist(filename, filelist, index);
-
-  while (fscanf(file, "%s", buffer) == 1) {
-    for(int i = 0; buffer[i]; i++){
-      buffer[i] = tolower(buffer[i]);
+    if(file == NULL) {
+        return -1;
     }
-    int hash = hashcode(buffer, MAX_LENGTH);
-    int word_loaded = is_word_loaded(buffer, index, &(htable_ptr->htable[hash]));
-    if(word_loaded == 0) {
-      add_word(buffer, index, &(htable_ptr->htable[hash]));
+
+    add_filelist(filename, filelist, index);
+
+    while (fscanf(file, "%s", buffer) == 1) {
+        for(i = 0; buffer[i]; i++){
+            buffer[i] = tolower(buffer[i]);
+        }
+        int hash = hashcode(buffer, MAX_LENGTH);
+        int word_loaded = is_word_loaded(buffer, index, &(htable_ptr->htable[hash]));
+        if(word_loaded == 0) {
+            add_word(buffer, index, &(htable_ptr->htable[hash]));
+        }
     }
-  }
 
-
-  if(feof(file)) {
-    fclose(file);
+    if(feof(file)) {
+        fclose(file);
 		return 0;
-  } else {
-    fclose(file);
+    }
+    else {
+        fclose(file);
 		return -2;
-  }
+    }
+
 }
 
 /**
@@ -116,19 +119,19 @@ int add_file(char filename[],listfile_entry * filelist, hash_table * htable_ptr)
    -1 if file not in table
     0 if file removed
 */
-int remove_file(char filename[], listfile_entry * filelist, hash_table * htable_ptr)
-{
-
-  for (int i = 0; i < MAX_FILES; i++) {
-    if(strcmp(filename, filelist[i].filename) == 0 && filelist[i].loaded == 1) {
-      filelist[i].loaded = 0;
-      for (int y = 0; y < MAX_ENTRIES; y++) {
-        remove_words_from_file(&(htable_ptr->htable[y].first_word), &(htable_ptr->htable[y].last_word), i);
-      }
-      return 0;
+int remove_file(char filename[], listfile_entry * filelist, hash_table * htable_ptr) {
+    int i = 0;
+    int y = 0;
+    for (i = 0; i < MAX_FILES; i++) {
+        if(strcmp(filename, filelist[i].filename) == 0 && filelist[i].loaded == 1) {
+            filelist[i].loaded = 0;
+            for (y = 0; y < MAX_ENTRIES; y++) {
+                remove_words_from_file(&(htable_ptr->htable[y].first_word), &(htable_ptr->htable[y].last_word), i);
+            }
+            return 0;
+        }
     }
-  }
-  return -1;
+    return -1;
 }
 
 /*
@@ -139,12 +142,12 @@ int remove_file(char filename[], listfile_entry * filelist, hash_table * htable_
 */
 void print_list(listfile_entry * filelist)
 {
-  for (int i = 0; i < MAX_FILES; i++) {
-      printf("%d -> Filename : %s, Status: %d\n", i, filelist[i].filename, filelist[i].loaded);
-  }
+    int i = 0;
+    for(i = 0; i < MAX_FILES; i++){
+        printf("Index %d  - File(%s) / Status(%d)\n", i, filelist[i].filename, filelist[i].loaded);
+    }
+
 }
-
-
 
 /**
    free file table
@@ -152,9 +155,8 @@ void print_list(listfile_entry * filelist)
 parameters :
    filelist   : pointer to table of files
 */
-void free_filelist(listfile_entry * filelist)
-{
-	free(filelist);
+void free_filelist(listfile_entry * filelist) {
+    free(filelist);
 }
 
 // ************************************************************************
@@ -166,9 +168,6 @@ void free_filelist(listfile_entry * filelist)
     1: if word aleady loaded and increments his times value
 		0: if word isn't already loaded
 */
-
-
-
 int is_word_loaded(char word[], int index, word_list* list) {
 
   word_entry * temp = list->first_word;
@@ -184,66 +183,42 @@ int is_word_loaded(char word[], int index, word_list* list) {
 }
 
 
-
-void remove_words_from_file(word_entry **head, word_entry **tail, int filelist_position)
-{
-
-  word_entry* current = *head;
-  word_entry* pre = NULL;
-
-  while(current != NULL && current->in_file == filelist_position) {
-    *head = current->next;
-    free(current);
-    current = *head;
-  }
-
-  while(current != NULL) {
-    while (current != NULL && current->in_file != filelist_position) {
-      pre = current;
-      current = current->next;
-    }
-    if (current->next == NULL && current->in_file == filelist_position) {
-      *tail = current;
-    }
-    if(current == NULL) return;
-
-    pre->next = current->next;
-    free(current);
-
-    current = pre->next;
-  }
-}
-
-
-
-
-
 void add_word(char* name, int fileindex, word_list* list) {
 
-  word_entry* newelt = (word_entry*)malloc(sizeof(word_entry));
+    word_entry * newelt = (word_entry*)malloc(sizeof(word_entry));
+    word_entry * prev = (word_entry*)malloc(sizeof(word_entry));
 
-	if(newelt != NULL) {
-		strcpy(newelt->word,name);
-		newelt->in_file = fileindex;
-		newelt->times = 1;
-		newelt->next = NULL;
+    if(newelt != NULL) {
+        strcpy(newelt->word, name);
+        newelt->in_file = fileindex;
+        newelt->times = 1;
+        newelt->next = NULL;
+    }
 
-		if (list->last_word != NULL) {
-			list->last_word->next = newelt;
-		}else {
-			list->first_word = newelt;
-		}
-		list->last_word = newelt;
+    if(list->first_word == NULL){
+        list->first_word = newelt;
+        return;
+    }
+
+    if(list->first_word != NULL && list->last_word == NULL ){
+        list->last_word = newelt;
+        list->first_word->next = list->last_word;
+        return;
+    }
+
+    if (list->last_word != NULL) {
+		list->last_word->next = newelt;
+        list->last_word = list->last_word->next;
+        return;
 	}
+
+
 }
-
-
 
 void add_filelist(char * filename, listfile_entry * filelist, int index) {
-  strcpy(filelist[index].filename, filename);
-  filelist[index].loaded = 1;
+    strcpy(filelist[index].filename, filename);
+    filelist[index].loaded = 1;
 }
-
 
 /*
 	returns:
@@ -266,4 +241,34 @@ int find_free_index(listfile_entry * list, char* filename) {
 		index++;
 	}
 	return -2;
+}
+
+void remove_words_from_file(word_entry **head, word_entry **tail, int filelist_position)
+{
+    word_entry* current = *head;
+    word_entry* pre = NULL;
+
+    while(current != NULL && current->in_file == filelist_position) {
+        *head = current->next;
+        free(current);
+        current = *head;
+
+    }
+
+    while(current != NULL) {
+        while (current != NULL && current->in_file != filelist_position) {
+            pre = current;
+            current = current->next;
+        }
+        if(current == NULL) return;
+        if (current->next == NULL && current->in_file != filelist_position) {
+            *tail = current;
+            free(current);
+        }
+
+        pre->next = current->next;
+        free(current);
+
+        current = pre->next;
+    }
 }
